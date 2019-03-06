724d5b29bc4f1b9b09ae902ef76c93008b10f28d
violet deng <dengviolet@gmail.com>
2019-03-06 16:33:05 +0800

    选项页面添加删除按钮

--- options.js
+++ options.js
@@ -1,7 +1,7 @@
 let page = document.getElementById('settingsDiv');
 chrome.storage.sync.get('settings', function (data) {
   if (!data.settings || !data.settings.length) {
-    addTemplate()
+    bindEvents(addTemplate())
     return
   }
   for (let item of data.settings) {
@@ -10,11 +10,12 @@ chrome.storage.sync.get('settings', function (data) {
     for (let input of inputs) {
       if (item[input.getAttribute('name')]) input.value = item[input.getAttribute('name')]
     }
+    bindEvents(container)
   }
 });
 
 document.getElementById('addButton').addEventListener('click', function () {
-  addTemplate()
+  bindEvents(addTemplate())
 })
 
 function addTemplate () {
@@ -23,11 +24,18 @@ function addTemplate () {
   return page.appendChild(wrapper.firstChild)
 }
 
+function bindEvents (container) {
+  container.querySelectorAll('.delete-button')[0].addEventListener('click', function () {
+    container.remove()
+  });
+}
+
 document.getElementById('saveButton').addEventListener('click', function () {
   let settings = [];
   for (let line of page.children) {
     let option = {};
     for (let inputWrapper of line.children) {
+      if (inputWrapper.className !== 'field') continue;
       let input = inputWrapper.children[0];
       option[input.getAttribute('name')] = input.value
     }
